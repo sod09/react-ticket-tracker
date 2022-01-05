c32a0a510bec189789ef712e8f73290e213c6ce7
Sasha O'Donovan <sasha.odonovan@gmail.com>
2022-01-05 14:33:56 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styles from "./App.module.scss";
+
+const App = () => {
+  return <p className={styles.para}>App Works...</p>;
+};
+
+export default App;
+
+// 1. change title in public/index.html
+// 2. Rename src/app.js to app.jsx
+// 3. delete code in app.css
+// 4. delete lovo.svg
+// 5. in index.css add global reset to index.css
+// 6. install sass
+// 7. rename App.css => App.module.scss
